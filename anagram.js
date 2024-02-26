8e7e684757bd76a121950f6ee0019b2119fb0ba5
Nguyen Van Kien <nkien6962@gmail.com>
2024-02-26 15:40:35 +0700

    anagram

---
+++ anagram.js
@@ -0,0 +1,8 @@
+const checkAnagram = (str1, str2) => {
+  const sortStr1 = str1.split("").sort().join("");
+  const sortStr2 = str2.split("").sort().join("");
+
+  return sortStr1 === sortStr2;
+};
+
+console.log(checkAnagram("listen", "silent"));

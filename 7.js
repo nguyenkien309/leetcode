82062081376a50ade34ea086d1e8ba4f8312ef36
Nguyen Van Kien <nkien6962@gmail.com>
2024-05-27 00:41:02 +0700

    chore: comment code

--- 7.js
+++ 7.js
@@ -1,4 +1,3 @@
-nums = -1234;
 const reverseNumber = (num) => {
   let reverse = 0;
   let check = num < 0 ? -num : num;
@@ -6,6 +5,11 @@ const reverseNumber = (num) => {
     reverse += (check % 10).toString();
     check = Math.floor(check / 10);
   }
+
+  // Hexadecimal representation 0x7fffffffff
+  // Maximum positive number stored in 32-bit
   return reverse < 0x7fffffff ? (nums < 0 ? -reverse : -(-reverse)) : 0;
 };
+
+nums = -1234;
 console.log(reverseNumber(nums));
